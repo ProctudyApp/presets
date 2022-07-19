30d137da3a95b484f1988b38c103361b310145a5
Fus1onDev <luke_s1220@yahoo.co.jp>
2022-07-19 22:18:30 +0900

    Add japanese message

--- validate.js
+++ validate.js
@@ -3,15 +3,15 @@ import { readFile } from "fs/promises";
 
 const schema = JSON.parse(await readFile("./schema.json"));
 const data = JSON.parse(await readFile("./presets.json"));
-//import schema from './schema.json';
-//import data from './presets.json';
 
 const ajv = new Ajv();
 
 const validate = ajv.compile(schema)
 const valid = validate(data)
 
-if (!valid) {
-  // バリデーションエラー
+if (valid) {
+  console.log("\x1b[32m✔︎ JSONの形式は問題ありません\x1b[0m");
+} else {
+  console.log("\x1b[31m✖️ 以下のエラーが発生しました:\x1b[0m")
   console.log(validate.errors);
 }
