a943054ac5c92e616cf0413cce08b6829b419995
Fus1onDev <luke_s1220@yahoo.co.jp>
2022-07-22 13:15:34 +0900

    Fix validate script

--- validate.js
+++ validate.js
@@ -6,12 +6,12 @@ const data = JSON.parse(await readFile("./presets.json"));
 
 const ajv = new Ajv();
 
-const validate = ajv.compile(schema)
-const valid = validate(data)
+const validate = ajv.compile(schema);
+const valid = validate(data);
 
 if (valid) {
-  console.log("\x1b[32m✔︎ JSONの形式は問題ありません\x1b[0m");
+  console.log("\x1b[32m✔︎ JSONの構造は問題ありません\x1b[0m");
 } else {
-  console.log("\x1b[31m✖️ 以下のエラーが発生しました:\x1b[0m")
+  console.log("\x1b[31m✖️ 以下のエラーが発生しました:\x1b[0m");
   console.log(validate.errors);
 }
